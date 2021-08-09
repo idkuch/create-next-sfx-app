6a2cb620b452eeecbc1a69c04d21d42655b718e4
Dawid Kucharski <6260692-dkuch@users.noreply.gitlab.com>
2021-08-09 18:12:18 +0200

    cli - added command to also setup git hooks

--- bin/cli.js
+++ bin/cli.js
@@ -16,6 +16,7 @@ const runCommand = (command) => {
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/davek-k/create-next-sfx-app ${repoName}`;
 const installDepsCommand = `cd ${repoName} && yarn`;
+const setupHuskyCommand = `yarn husky install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
@@ -32,3 +33,17 @@ if (!installedDeps) {
 }
 
 console.log(`Successfully installed dependencies for ${repoName}`);
+
+console.log(`Setting up git hooks for ${repoName}`);
+
+const setupedHusky = runCommand(setupHuskyCommand);
+
+if (!setupedHusky) {
+	process.exit(-1);
+}
+
+console.log('Successfully setuped hooks.');
+
+console.log(
+	"Everything done. Run 'cd ${repoName} && yarn dev' to start developing."
+);
